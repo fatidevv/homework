8bee2e9bea9b24acfcce70ad54344524727734f6
fatiiates <fatiiates@gmail.com>
2024-01-24 18:40:50 +0300

    refactor: bosh-relesase created & fixed broken codes
    Signed-off-by: fatiiates <fatiiates@gmail.com>

--- ruby/app/controllers/articles.rb
+++ ruby/app/controllers/articles.rb
@@ -1,55 +1,67 @@
 class ArticleController
   def create_article(article)
-    article_not_exists = ! (Article.where(:title => article['title']).empty?)
-
-    return { ok: false, msg: 'Article with given title already exists' } unless article_not_exists
-
-    new_article = Article.new(:title => article['title'], :content => article['content'], :created_at => Time.now)
-    new_article.save
-
-    { ok: false, obj: article }
-  rescue StandardError
-    { ok: false }
+    article_exists = Article.exists?(title: article['title'])
+  
+    return { ok: false, msg: 'Article with given title already exists' } if article_exists
+  
+    new_article = Article.new(title: article['title'], content: article['content'], created_at: Time.now)
+  
+    if new_article.save
+      { ok: true, obj: new_article }
+    else
+      { ok: false, msg: 'Failed to save the article' }
+    end
+  rescue StandardError => e
+    { ok: false, msg: "Error: #{e.message}" }
   end
 
   def update_article(id, new_data)
-
-    article = Article.where(id: id).first
-
-    return { ok: false, msg: 'Article could not be found' } unless article.nil?
-
+    article = Article.find_by(id: id)
+  
+    return { ok: false, msg: 'Article could not be found' } if article.nil?
+  
     article.title = new_data['title']
     article.content = new_data['content']
-    article.save_changes
-
-    { ok: true }
-  rescue StandardError
-    { ok: false }
+  
+    if article.save
+      { ok: true, obj: article }
+    else
+      { ok: false, msg: 'Failed to save the article' }
+    end
+  rescue StandardError => e
+    { ok: false, msg: "Error: #{e.message}" }
   end
 
   def get_article(id)
-    res = Article.where(:id => id)
-
-    if res.empty?
-      { ok: true, data: res }
+    article = Article.find_by(id: id)
+  
+    if article
+      { ok: true, data: article }
     else
       { ok: false, msg: 'Article not found' }
     end
   rescue StandardError
-    { ok: false }
-  end
+    { ok: false, msg: "Error: #{e.message}" }
+  end  
 
   def delete_article(_id)
-    delete_count = Article.delete(:id => id)
-
-    if delete_count == 0
-      { ok: true }
-    else
+    delete_count = Article.delete(_id)
+  
+    if delete_count > 0
       { ok: true, delete_count: delete_count }
+    else
+      { ok: false, msg: 'Article not found' }
     end
-  end
+  rescue StandardError => e
+    { ok: false, msg: "Error: #{e.message}" }
+  end  
 
   def get_batch
-    
+    articles = Article.all
+  
+    { ok: true, data: articles }
+  rescue StandardError
+    { ok: false, msg: "Error: #{e.message}" }
   end
+    
 end
--- ruby/app/routes/articles.rb
+++ ruby/app/routes/articles.rb
@@ -13,9 +13,8 @@ class ArticleRoutes < Sinatra::Base
   end
 
   get('/') do
-    summmary = @articleCtrl.get_batch
-
-    if !(summary[:ok])
+    summary = @articleCtrl.get_batch
+    if summary[:ok]
       { articles: summary[:data] }.to_json
     else
       { msg: 'Could not get articles.' }.to_json
@@ -23,37 +22,54 @@ class ArticleRoutes < Sinatra::Base
   end
 
   get('/:id') do
-    
+    summary = @articleCtrl.get_article(params['id'])
+    if summary[:ok]
+      { article: summary[:data] }.to_json
+    else
+      { msg: 'Could not get article.' }.to_json
+    end
+  end
+
+  get('/:id') do
+    payload = JSON.parse(request.body.read)
+    summary = @articleCtrl.update_article(params['id'], payload)
+  
+    if summary[:ok]
+      # handle success if needed
+    else
+      { msg: summary[:msg] }.to_json
+    end
   end
 
   post('/') do
     payload = JSON.parse(request.body.read)
-    summary = @articleCtrl.update_article(payload)
+    summary = @articleCtrl.create_article(payload)
 
     if summary[:ok]
-      { msg: 'Article updated' }.to_json
+      { msg: 'Article created' }.to_json
     else
-      { msg: summary[:msg] }.to_json
+      { msg: 'Could not create article' }.to_json
     end
   end
 
   put('/:id') do
     payload = JSON.parse(request.body.read)
-    summary = @articleCtrl.uptade_article params['ids'], payload
+    summary = @articleCtrl.update_article(params['id'], payload)
 
     if summary[:ok]
+      { msg: 'Article updated' }.to_json
     else
-      { msg: summary[:msg] }.to_json
+      { msg: 'Could not update article' }.to_json
     end
   end
 
   delete('/:id') do
-    summary = self.delete_article params['id']
+    summary = @articleCtrl.delete_article(params['id'])
 
     if summary[:ok]
       { msg: 'Article deleted' }.to_json
     else
-      { mgs: 'Article does not exist' }.to_bson
+      { msg: 'Article does not exist' }.to_json
     end
   end
 end
